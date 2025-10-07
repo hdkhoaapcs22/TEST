cf94be41cbaa444550dbc7dc6d5824fc067556f5
hdkhoaapcs22 <hdkhoa22@apcs.fit.edu.vn>
2025-10-07 23:07:49 +0700

    feature/divide

--- main.js
+++ main.js
@@ -10,5 +10,8 @@ const sub = (x,y) => {
 
 const multiple = (x,y) => x * y
 
+const divide = (x,y) => x/y
+
 console.log(add(1, 2)); // 3
-console.log(sub(2,1))
+console.log(sub(2,1));
+console.log(divide(4,2));
